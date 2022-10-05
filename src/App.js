19df72bfec4ff3d9e09e5a40f52b62efae6de70d
alois27 <alois_sailer@yahoo.com.br>
2022-10-05 20:52:58 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,63 @@ import "./styles.css";
 
 import Header from "./components/Header";
 import Section from "./components/Section";
+import ListItem from "./components/ListItem";
+
+const gamesListData = [
+  {
+    url: "https://www.twitch.tv/directory/game/League%20of%20Legends",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg",
+    alt: "Imagem do jogo League of Legends",
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/VALORANT",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg",
+    alt: "Imagem do jogo Valorant",
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Minecraft",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg",
+    alt: "Imagem do jogo Minecraft",
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Teamfight%20Tactics",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg",
+    alt: "Imagem do jogo TFT",
+  },
+];
+
+const channelListData = [
+  {
+    url: "https://www.twitch.tv/alanzoka",
+    imageUrl:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/15cec952-c1ba-4ff8-a79c-53c2fa5bd269-profile_image-150x150.png",
+    alt: "Imagem de Alanzoka",
+  },
+  {
+    url: "https://www.twitch.tv/cellbit",
+    imageUrl:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/cb885149-f7f7-48f0-99ae-fe0a386ed3e2-profile_image-150x150.png",
+    alt: "Imagemd de Cellbit",
+  },
+];
+
+const socialListData = [
+  {
+    url: "https://twitter.com/AloisSailer1",
+    imageUrl: "/assets/twitter.svg",
+    alt: "Twitter do Alois Sailer",
+  },
+  {
+    url: "https://www.instagram.com/alois_sailer/",
+    imageUrl: "/assets/instagram.svg",
+    alt: "Instagram do Alois Sailer",
+  },
+  {
+    url: "https://www.youtube.com/channel/UC-Oa_BhD1igKS5i-LPv4P8w",
+    imageUrl: "/assets/youtube.svg",
+    alt: "Youtube do Alois Sailer",
+  },
+];
 
 export default function App() {
   return (
@@ -11,11 +68,49 @@ export default function App() {
         <Section
           title="Meus Jogos"
           subtitle="Os games que eu mais curto jogar!"
-        />
+          className="games-list"
+        >
+          {gamesListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
+
         <Section
           title="Canais e Streamers"
-          subtitle="Lista de canais e transmissçoes que não perco!"
-        />
+          subtitle="Lista de canais e transmissões que nâo perco!"
+          className="channel-list"
+        >
+          {channelListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
+        <Section
+          title="Minhas redes"
+          subtitle="Se conecta comigo agora mesmo!"
+          className="social-list"
+        >
+          {socialListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
       </main>
     </div>
   );
