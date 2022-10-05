b2e6d037a175b5643fa51cfe78e9988bfff558cc
alois_sailer <alois_sailer@yahoo.com.br>
2022-10-04 23:03:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+import Header from "./components/Header";
+import Section from "./components/Section";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Section
+          title="Meus Jogos"
+          subtitle="Os games que eu mais curto jogar!"
+        />
+        <Section
+          title="Canais e Streamers"
+          subtitle="Lista de canais e transmissçoes que não perco!"
+        />
+      </main>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <img
+          src="https://github.com/AloisSailer.png"
+          alt="Foto do Alois Sailer"
+        />
+      </div>
+    </header>
+  );
+}
